93457963451455e0575849d1719eea1069ffaecf
jiangxingqi <jxq0816@126.com>
2017-06-27 11:07:18 +0800

    get set

--- persistence/src/main/java/com/inter3i/sun/persistence/dataimport/ResponseBean.java
+++ persistence/src/main/java/com/inter3i/sun/persistence/dataimport/ResponseBean.java
@@ -19,4 +19,28 @@ public class ResponseBean {
     private boolean success = true;
     private String errorCode;
     private String errorMsg;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
 }
