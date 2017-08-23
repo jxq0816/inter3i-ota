e2a815c31c4544d0b9aebccc99e1545be269795d
jiangxingqi <jxq0816@126.com>
2017-08-23 11:16:58 +0800

    Tomcat启动 禁用mongo默认配置

--- ota-api/src/main/java/com/inter3i/sun/api/ota/v1/Application.java
+++ ota-api/src/main/java/com/inter3i/sun/api/ota/v1/Application.java
@@ -10,20 +10,15 @@
 package com.inter3i.sun.api.ota.v1;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 
 @ComponentScan
-@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
-@EnableConfigurationProperties
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class,MongoDataAutoConfiguration.class})
 //在部署到外部的Tomcat时，需要将classpath的引入文件去掉，因为在web.xml已经配置过一次了
-@ImportResource("classpath:dispatcher-servlet.xml")
+//@ImportResource("classpath:dispatcher-servlet.xml")
 public class Application{
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
